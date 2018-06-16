ac9bc31e619571aa4e137c6e45b5a6fa030479e8
fabianodc <37005003+fabianodc@users.noreply.github.com>
2018-06-16 10:13:30 -0300

    Maicon com senha

--- hello.js
+++ hello.js
@@ -13,7 +13,7 @@ app.use(session({
 }))
 
 app.get('/', function (req, res) {
- // if(req.session.auth==true) {
+  if(req.session.auth==true) {
 	  if (req.session.count) {
 		req.session.count++
 		res.setHeader('Content-Type', 'text/html')
@@ -24,9 +24,9 @@ app.get('/', function (req, res) {
 		req.session.count = 1
 		res.end('welcome to the session demo. refresh!')
 	  }
- // } else {
-	//    res.send('you must login first');
- // }
+  } else {
+	    res.send('you must login first');
+  }
 });
 
 app.post('/login', upload.array(), function (req, res, next) {
@@ -35,7 +35,7 @@ app.post('/login', upload.array(), function (req, res, next) {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html><head><meta charset="utf-8"></head><body><p>Você já está logado</p></body></html>');
     res.end();
-  } else if(req.body.nome=="maicon" && req.body.senha=="teste") {
+  } else if(req.body.nome=="fabiano" && req.body.senha=="teste") {
 		req.session.auth= true;
     	res.end('welcome to the session demo. refresh!');
   } else {
